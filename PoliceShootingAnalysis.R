ae9148485b6de286ef529376f84636ac82628ca8
mrajendiran <monica.rajen@gmail.com>
2017-02-24 12:34:24 -0500

    Police Shooting Analysis

---
+++ PoliceShootingAnalysis.R
@@ -0,0 +1,123 @@
+setwd("~/Documents/MSDS/1_Machine Learning/Projects/data-police-shootings")
+
+suppressMessages({
+  library(dplyr)
+  library(readr)
+  library(lubridate)
+  library(ggplot2)
+  library(ggthemes)
+})
+
+# Read in the CSV file
+shootings <- read_delim("fatal-police-shootings-data.csv", delim=',', progress=F)
+shootings %>% glimpse
+
+# Factor variables except for date and age
+shootings[] <- lapply(shootings, factor)
+shootings$date  <- ymd(shootings$date)
+shootings$age <- as.numeric(shootings$age)
+shootings %>% glimpse
+
+# Check for NAs
+sum(is.na(shootings)) # 207 NAs
+shootings %>%
+  summarise_each(funs(sum(is.na(.))))
+#    id  name  date manner_of_death armed   age gender  race  city state signs_of_mental_illness threat_level  flee body_camera
+# <int> <int> <int>           <int> <int> <int>  <int> <int> <int> <int>                   <int>        <int> <int>       <int>
+#     0     0     0               0     7    46      1   116     0     0                       0            0    37           0
+
+# Inspect DF with complete cases
+shootings_complete <- shootings[complete.cases(shootings), ] 
+shootings_complete %>% glimpse # 1,959 cases from 2,126
+
+# Separate cases between 2015 and 2016 (1820 total cases)
+shootings_complete %>%
+  filter(year(date) == 2015) -> shootings2015 # 947 obs
+shootings_complete %>%
+  filter(year(date) == 2016) -> shootings2016 # 873 obs
+# Cases for 2017
+shootings_complete %>%
+  filter(year(date) == 2017) -> shootings2017 # 139 obs
+
+# Descriptive Insights
+
+# Gender
+shootings_complete %>%
+  group_by(gender) %>% 
+  summarise(count = n_distinct(id)) -> shootings_gender
+shootings_gender %>% glimpse
+# gender <chr> "F", "M"
+# count  <int> 81, 1878
+
+# Age
+shootings_complete %>%
+  group_by(age) %>% 
+  summarise(count = n_distinct(id)) -> shootings_age
+ggplot(shootings_age, aes(x=count, y=age)) + 
+         geom_bar(stat="identity") +
+         labs(list(x="Count", y="Age", title="Shootings by Age"))
+
+# Manner of Death
+shootings_complete %>%
+  group_by(manner_of_death) %>% 
+  summarise(count = n_distinct(id)) -> shootings_death
+shootings_death %>% glimpse
+# manner_of_death <chr> "shot", "shot and Tasered"
+# count           <int> 1823, 136
+
+# Threat Level 
+shootings_complete %>%
+  group_by(threat_level) %>% 
+  summarise(count = n_distinct(id)) -> shootings_threat
+shootings_threat %>% glimpse
+# threat_level <chr> "attack", "other", "undetermined"
+# count        <int> 1284, 568, 107
+
+# Flee
+shootings_complete %>%
+  group_by(flee) %>% 
+  summarise(count = n_distinct(id)) -> shootings_flee
+shootings_flee %>% glimpse
+# flee  <chr> "Car", "Foot", "Not fleeing", "Other"
+# count <int> 299, 243, 1341, 76
+
+# City
+shootings_complete %>%
+  group_by(city) %>% 
+  summarise(count = n_distinct(id)) -> shootings_city
+ggplot(shootings_city %>% filter(count > 10), aes(x=city, y=count, fill=city)) + 
+  geom_bar(stat="identity") +
+  labs(list(x="Count", title="Shootings by City")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  guides(fill=FALSE)
+
+# State
+shootings_complete %>%
+  group_by(state) %>% 
+  summarise(count = n_distinct(id)) -> shootings_state
+shootings_state %>% glimpse
+ggplot(shootings_state %>% filter(count > 20), aes(x=state, y=count, fill=state)) + 
+  geom_bar(stat="identity") +
+  labs(list(x="Count", title="Shootings by State")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  guides(fill=FALSE)
+
+# Signs of Mental Illness
+shootings_complete %>%
+  group_by(signs_of_mental_illness) %>% 
+  summarise(count = n_distinct(id)) -> shootings_illness
+shootings_illness %>% glimpse
+# signs_of_mental_illness <chr> "False", "True"
+# count                   <int> 1473, 486
+
+# Armed
+shootings_complete %>%
+  group_by(armed) %>% 
+  summarise(count = n_distinct(id)) -> shootings_armed
+shootings_armed %>% glimpse
+ggplot(shootings_armed %>% filter(count > 5), aes(x=armed, y=count)) + 
+  geom_bar(stat="identity") +
+  labs(list(x="Count", title="Shootings by Armed Status")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
